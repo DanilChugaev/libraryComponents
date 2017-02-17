9011388c50fe5cc8da7b4ddc3f30a43fe52564d8
Чугаев Данил <dmchugaev@gmail.com>
2017-02-17 12:18:17 +0500

    Добавил регулярное выражение, для проверки на кавычки в имени файла

--- app/js/custom/preview.js
+++ app/js/custom/preview.js
@@ -1,12 +1,13 @@
 (function() {
     var uploadElements  = document.getElementById('upload'),    /*инпут типа файл*/
-        previewImg      = document.getElementById('preview-img'),
-        previewText     = document.getElementById('preview-text'),
-        formData        = new FormData(),
+        previewImg      = document.getElementById('preview-img'),/*тут выводятся превьюшки изображений*/
+        previewText     = document.getElementById('preview-text'),/*тут выводятся наименования файлов*/
+        formData        = new FormData(),   /*объект формДата для отправки файлов на сервер*/
         form            = document.getElementById('formWithFile'), /*форма*/
         uploadButton    = document.getElementById('upload-button'), /*кнопка загрузки*/
         xhr             = new XMLHttpRequest();
 
+    /*событие загрузки файлов*/
     uploadElements.addEventListener("change", function(e) {
         var arr = [].slice.call(this.files);
 
@@ -16,11 +17,14 @@
         arr.forEach(function (item, i, arr) {
             preview(item);
 
-            formData.append('file[' + item.name + ']', item, item.name);
+            var newName = rename(item.name,1);
+
+            formData.append('file[' + newName + ']', item, newName);
 
         });
     });
 
+    /*отправка формы*/
     form.onsubmit = function(event) {
         event.preventDefault();
 
@@ -38,12 +42,15 @@
         xhr.send(formData);
     };
 
+    /*удаление файлов*/
     deleteFiles = function (obj) {
-        var fileName = obj.getAttribute('name'),
+        var fileName = rename(obj.getAttribute('name'),2),
             img      = previewImg.childNodes;
 
         img.forEach(function(item, i, arr) {
-            if(item.name == fileName) {
+            var newName = rename(item.name,1);
+
+            if(newName == fileName) {
                 item.remove();
             }
         });
@@ -52,7 +59,10 @@
         formData.delete('file[' + fileName + ']');
     };
 
+    /*показ превью и наименования файлов*/
     function preview(file) {
+        var newName = rename(file.name,1);
+
         /*тут также можно сделать проверку на соответствие типу файла*/
         if ( file.type.match(/image.*/) ) {
             var reader = new FileReader(), img;
@@ -60,7 +70,7 @@
             reader.addEventListener("load", function(event) {
                 img = document.createElement('img');
                 img.src = event.target.result;
-                img.name = file.name;
+                img.name = newName;
                 previewImg.appendChild(img);
             });
 
@@ -68,8 +78,16 @@
         }
 
         var li = document.createElement('li');
-        li.innerHTML = "<span>" + file.name + "</span>" + " " + "<button type='button' name='" + file.name + "' onclick='deleteFiles(this)'>X</button>";
+        li.innerHTML = "<span>" + newName + "</span>" + " " + "<button type='button' name='" + newName + "' onclick='deleteFiles(this)'>X</button>";
         previewText.appendChild(li);
+    }
+
+    /*замена регулярным выражением кавычек в имени*/
+    function rename(name,number) {
+        switch(number) {
+            case 1:  return name.replace(/'/g, '&#34;'); break;
+            case 2:  return name.replace(/"/g, '&#34;'); break;
+        }
     }
 })();
 
