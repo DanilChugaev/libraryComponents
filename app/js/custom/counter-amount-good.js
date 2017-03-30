5157532c6e67bc290b5b4cc6c06b818cdb3f89b0
Чугаев Данил <dmchugaev@gmail.com>
2017-03-30 16:55:08 +0500

    обновил

--- app/js/custom/counter-amount-good.js
+++ app/js/custom/counter-amount-good.js
@@ -1,26 +1,32 @@
 (function() {
     /*
-     *    счетчик
+     *    счетчик товаров
+     *    <input type="button" class="less-amount">
+     *    <input type="text" class="amount-good" value='1'>
+     *    <input type="button" class="more-amount">
      */
-    var amount = document.getElementById('amount-good'),
-        lessAmount = document.getElementById('less-amount'),
-        moreAmount = document.getElementById('more-amount');
+    var amount = document.getElementsByClassName('amount-good'),
+        lessAmount = document.getElementsByClassName('less-amount'),
+        moreAmount = document.getElementsByClassName('more-amount');
 
-    amount.addEventListener('keydown', function(event) {
-        if( (isNaN(event.key) || event.key == " ") && event.key != "Backspace" ) {
-            event.preventDefault();
-        }
-    });
+    for(var i = 0; i < amount.length; i++) {
+        amount[i].addEventListener('keydown', function(event) {
+            if( (isNaN(event.key) || event.key == " ") && event.key != "Backspace" ) {
+                event.preventDefault();
+            }
+        });
+    }
+    for(var i = 0; i < lessAmount.length; i++) {
+        lessAmount[i].addEventListener('click', function(event) {
+            if(this.nextSibling.nextSibling.value == 1)
+                return false;
 
-    lessAmount.addEventListener('click', function(event) {
-
-        if(amount.value == 1)
-            return false;
-
-        amount.value--;
-    });
-
-    moreAmount.addEventListener('click', function(event) {
-        amount.value++;
-    });
+            this.nextSibling.nextSibling.value--;
+        });
+    }
+    for(var i = 0; i < moreAmount.length; i++) {
+        moreAmount[i].addEventListener('click', function(event) {
+            this.previousSibling.previousSibling.value++;
+        });
+    }
 })();
